59c190ba9b64e3e55f7ff1e15da650e729331bb0
Logan <37604784+imlogang@users.noreply.github.com>
2024-04-15 12:31:55 -0500

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Logan Godsey on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
